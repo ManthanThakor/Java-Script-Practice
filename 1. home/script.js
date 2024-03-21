c6e030b2430e013ce60b5e4ff4d511a7812bc76c
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-21 16:10:05 +0530

    home 2

--- 1. home/script.js
+++ 1. home/script.js
@@ -2,3 +2,8 @@ document.write("hello world <br>");
 document.write("<i>welcome to the MRzone</i> <br>");
 document.write("<b>how are you?</b>");
 console.log("how are you?");
+
+document.addEventListener('click', function() {
+  console.log('how are you?');
+  document.getElementById('demo').innerHTML = new Date();
+});
