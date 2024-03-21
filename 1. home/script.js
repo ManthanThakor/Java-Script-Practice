ea18a1df4541f0d837e4d2880f8c28a684e9a9f9
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-21 16:11:50 +0530

    home 3

--- 1. home/script.js
+++ 1. home/script.js
@@ -1,8 +1,15 @@
+
+// how to add html tags in javascript
+
 document.write("hello world <br>");
 document.write("<i>welcome to the MRzone</i> <br>");
 document.write("<b>how are you?</b>");
+
+// console.log
+
 console.log("how are you?");
 
+// eventlistener
 document.addEventListener('click', function() {
   console.log('how are you?');
   document.getElementById('demo').innerHTML = new Date();
