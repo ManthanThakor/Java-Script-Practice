64e81d67e47d7972ee5bcf3d320d9de00d3ab607
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-21 20:42:41 +0530

    syntax

--- 4. JavaScript-Statements,syntax,comments/index.js
+++ 4. JavaScript-Statements,syntax,comments/index.js
@@ -3,18 +3,39 @@
 
 // *************************JS statements****************************
 
+// JavaScript Programs
+// A computer program is a list of "instructions" to be "executed" by a computer.
+// In a programming language, these programming instructions are called statements.
+// A JavaScript program is a list of programming statements.
+
+// JavaScript Statements
+// JavaScript statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments.
 
 let x, y, z;  // Statement 1
 x = 5;        // Statement 2
 y = 6;        // Statement 3
 z = x + y;    // Statement 4
+document.getElementById("demo").innerHTML ="The value is " + (x+z) + ".";  
 
-document.getElementById("demo").innerHTML =
-"The value is " + z + ".";  
+document.getElementById("demoo").innerHTML = "Hello Dolly.";
+
+// more theory from w3school
+
 // ----------------------------------------------------------------
 
 
 // *************************JS Syntax****************************
+
+// JavaScript Values
+// The JavaScript syntax defines two types of values:
+
+// Fixed values
+// Variable values
+// Fixed values are called Literals.
+
+// Variable values are called Variables.
+
 
 
 // ----------------------------------------------------------------
