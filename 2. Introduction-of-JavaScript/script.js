f8b781cf2718fb37de97e6168355d79dde53cace
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-21 19:03:10 +0530

    INTRODUCTION 1

--- 2. Introduction-of-JavaScript/script.js
+++ 2. Introduction-of-JavaScript/script.js
@@ -10,43 +10,17 @@
 
 // **************************************JavaScript Can Change HTML Content********************************************
 
-//--------------------------------------------- PREFER W3SCHOOL--------------------------------------------------------
-
-
-// One of many JavaScript HTML methods is getElementById().
-// The example below "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript":
-// Example
-document.getElementById("demo").innerHTML = "Hello JavaScript";
-
-// JavaScript accepts both double and single quotes:
-// Example
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
-
-// JavaScript Can Change HTML Attribute Values
-// In this example JavaScript changes the value of the src (source) attribute of an <img> tag:
-
-
-
-// JavaScript Can Change HTML Styles (CSS)
-// Changing the style of an HTML element, is a variant of changing an HTML attribute:
-// Example
-document.getElementById("demo").style.fontSize = "35px";
-
-// JavaScript Can Hide HTML Elements
-// Hiding HTML elements can be done by changing the display style:
-// Example
-document.getElementById("demo").style.display = "none";
-
-// JavaScript Can Show HTML Elements
-// Showing hidden HTML elements can also be done by changing the display style:
-// Example
-document.getElementById("demo").style.display = "block";
-
-
-// **************************************HOW TO WRITE********************************************
+// **************************************HOW TO WRITE JS********************************************
 
 // The <script> Tag
+// JavaScript in <head> or <body>
+// You can place any number of scripts in an HTML document.
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+// ----------------------------------------------------------------
 // In HTML, JavaScript code is inserted between <script> and </script> tags.
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
+//  or make new file with .js extension like <script src="myScript.js"></script>
+
+// External JavaScript
+function myFunction() {
+    document.getElementById("external").innerHTML = "Paragraph changed.";
+  }
