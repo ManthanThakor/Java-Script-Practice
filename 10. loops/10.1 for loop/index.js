71bf0217c14ea8fe17afcf336f8097428ff54e15
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-22 14:35:20 +0530

    loops 1

---
+++ 10. loops/10.1 for loop/index.js
@@ -0,0 +1,19 @@
+// ================================================================
+// https://youtu.be/VlPiVmYuoqw?si=rpfR_8eWHA6Zlob6
+
+// for Loop:
+
+// The for loop is used when you know the number of iterations needed.
+
+// Example: Printing numbers from 1 to 5 using a for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(`COUNT ${i}`); // use ${} for ___**template literal**_____
+}
+console.log("loop finished");
+// Explanation:
+// The loop starts with initializing i to 1.
+// It continues as long as i is less than or equal to 5.
+// After each iteration, i is incremented by 1.
+// In each iteration, the value of i is printed to the console.
+
+// ================================================================
