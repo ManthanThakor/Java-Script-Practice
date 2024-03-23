e460de8fcc33301cd51be1795d14c6b1059574a3
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-23 14:06:21 +0530

    string

---
+++ 13. string/index.js
@@ -0,0 +1,10 @@
+
+
+// Strings in JS
+
+// Strings are for storing text
+// Strings are written with quotes
+// String is a sequence of characters used to represent text
+
+let str = "WELCOME TO THE PIXEL WORLD";
+console.log(str);
