1eb1fe871ecee79c37b3dd3815488ef365804cf8
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-23 16:56:06 +0530

    string \2

--- 11. JS debugging/index.js
+++ 11. JS debugging/index.js
@@ -9,3 +9,13 @@
 // Normally (otherwise follow the steps at the bottom of this page), you activate debugging in your browser with the F12 key, and select "Console" in the debugger menu.
 
 console.log("debugger activated");
+
+// Six other escape sequences are valid in JavaScript:
+
+// Code	Result
+// \b	Backspace
+// \f	Form Feed
+// \n	New Line
+// \r	Carriage Return
+// \t	Horizontal Tabulator
+// \v	Vertical Tabulator
