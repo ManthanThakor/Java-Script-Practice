eb838620cb97712f61fa8c77f932b06ff522d6f9
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-26 12:12:54 +0530

    Array 2

--- 19. Array/index.js
+++ 19. Array/index.js
@@ -1,1 +1,40 @@
 
+// ****************************************************************ARRAYS ****************************************************************
+
+
+/* ARRAY IS A COLLECTION OF ITEMS
+An array is a special variable, which can hold more than one value:*/
+
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+/* Why Use Arrays?
+If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
+
+let car1 = "Saab";
+let car2 = "Volvo";
+let car3 = "BMW"; */
+
+//________________________________syntax________________________________
+
+/* Creating an Array
+Using an array literal is the easiest way to create a JavaScript Array.
+
+Syntax: */
+
+// const array_name = [item1, item2, ...];  It is a common practice to declare arrays with the const keyword.
+
+// ________________________________________________________________________________________________________________________________
+
+
+// Spaces and line breaks are not important. A declaration can span multiple lines:
+
+// Example
+const carss = [
+  "buggati",
+  "audi",
+  "neno"
+];
+
+console.log(carss);
+
