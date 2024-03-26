dd2a5de903290b3ffc02e9ea20b41d18094f654c
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-26 09:45:04 +0530

    Alert box 2

---
+++ 16. alertbox/script.js
@@ -0,0 +1,17 @@
+
+
+// _______________Alert box _______________
+
+var a= 10;
+var b= 20;
+
+if(a>b){
+    alert('a is greater than b '+ a );
+}
+else {
+    alert('b is greater than a ' + b);
+
+}
+
+
+alert('You are in danger mode');
