8a4fd4d2259381db69b473edc1288bc72a8ca34d
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-27 10:35:51 +0530

    Array 7

---
+++ 19. Array/array_method.js
@@ -0,0 +1,43 @@
+// Basic Array Methods
+// Array length
+// Array toString()
+// Array at()
+// Array join()
+// Array pop()
+// Array push()
+// Search Methods
+// Sort Methods
+// Iteration Methods	
+// Array shift()
+// Array unshift()
+// Array delete()
+// Array concat()
+// Array copyWithin()
+// Array flat()
+// Array splice()
+// Array toSpliced()
+// Array slice()
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Arrays Methods @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+// Array concat()
+// concat(): Joins two or more arrays and returns a new array.
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const newArray = array1.concat(array2);
+console.log(newArray,"\n"); // Output: [1, 2, 3, 4, 5, 6]
+
+// JavaScript Array length
+// The length property returns the length (size) of an array:
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(size,"\n"); // Output: 4
+
+
+
+
+
