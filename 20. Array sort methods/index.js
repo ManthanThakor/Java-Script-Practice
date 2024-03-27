4ccc478b031dd8d1020c355be193828146cb2780
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-27 15:08:03 +0530

    Array 16

---
+++ 20. Array sort methods/index.js
@@ -0,0 +1,17 @@
+
+
+// @@@@@@@@@@@@@@@@@@ Alpabetic Sort @@@@@@@@@@@@@@@@@@
+
+// Array sort()
+// Array reverse()
+// Array toSorted()
+// Array toReversed()
+
+// @@@@@@@@@@@@@@@@@@ Numeric Sort @@@@@@@@@@@@@@@@@@
+
+// Numeric Sort
+// Random Sort
+// Math.min()
+// Math.max()
+// Home made Min()
+// Home made Max()
