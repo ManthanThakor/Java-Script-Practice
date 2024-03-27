4ca00572f399bdd04f08a8f78b258610810f5183
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-27 10:52:02 +0530

    Array 9

--- 19. Array/array_method.js
+++ 19. Array/array_method.js
@@ -1,17 +1,17 @@
 // Basic Array Methods
-// Array length
-// Array toString()
-// Array at()
-// Array join()
-// Array pop()
-// Array push()
+// Array length ********************************
+// Array toString()********************************
+// Array at()********************************
+// Array join()********************************
+// Array pop()********************************
+// Array push()********************************
 // Search Methods
 // Sort Methods
 // Iteration Methods	
 // Array shift()
 // Array unshift()
 // Array delete()
-// Array concat()
+// Array concat() ********************************
 // Array copyWithin()
 // Array flat()
 // Array splice()
@@ -52,3 +52,46 @@ console.log(aaa,"\n"); // Output: "Banana,Orange,Apple,Mango"
 const fruiats = ["Banana", "Orange", "Apple", "Mango"];
 let fruiaaat = fruiats.at(2);
 console.log(fruiaaat,"\n"); // Output: "Apple"
+
+
+// JavaScript Array join()
+// The join() method also joins all array elements into a string.
+// It behaves just like toString(), but in addition you can specify the separator:
+
+const fruitas = ["Banana", "Orange", "Apple", "Mango"];
+let aaaa = fruitas.join(",");
+let aaaaa = fruitas.join("*");
+console.log(aaaa,"\n"); // Output: "Banana,Orange,Apple,Mango"
+console.log(aaaaa,"\n"); // Output: "Banana*Orange*Apple*Mango"
+
+// ___________________Popping and Pushing___________________
+// When you work with arrays, it is easy to remove elements and add new elements.
+// This is what popping and pushing is:
+// Popping items out of an array, or pushing items into an array.
+
+// JavaScript Array pop()
+// The pop() method removes the last element from an array:
+
+
+const fruitss = ["Banana", "Orange", "Apple", "Mango"];
+let xx = fruitss.pop();
+console.log(xx,"\n"); // Output: "Mango"
+console.log(fruitss,"\n");
+
+// The pop() method returns the value that was "popped out":
+
+// JavaScript Array push()
+// The push() method adds a new element to an array (at the end):
+
+const fruiaats = ["Banana", "Orange", "Apple", "Mango"];
+fruiaats.push("Kiwi");
+
+console.log(fruiaats,"\n"); // Output: ["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+
+// The push() method returns the new array length
+
+const frauits = ["Banana", "Orange", "Apple", "Mango"];
+let length = frauits.push("Kiwii");
+
+console.log(length,"\n"); // Output: 5
+console.log(frauits,"\n");
