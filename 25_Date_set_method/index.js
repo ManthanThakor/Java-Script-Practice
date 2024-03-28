a84a761a530367cdc6ef001cf5ffcfadcae700a8
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-28 09:40:14 +0530

    Date formate 6

--- 25_Date_set_method/index.js
+++ 25_Date_set_method/index.js
@@ -21,3 +21,43 @@ The setFullYear() method sets the year of a date object. In this example to 2020
 Example */
 const d = new Date();
 console.log(d.setFullYear(2013));
+
+/* The setHours() Method
+The setHours() method sets the hour of a date object. In this example to 12:
+
+Example */
+
+const d33 = new Date();
+console.log(d33.setHours(12));
+
+/* The setMilliseconds() Method
+The setMilliseconds() method sets the milliseconds of a date object. In this example to 300:
+
+Example */
+
+const d6 = new Date();
+console.log(d6.setMilliseconds(300));
+
+/* The setMinutes() Method
+The setMinutes() method sets the minutes of a date object. In this example to 30:
+
+Example */
+
+const d5 = new Date();
+console.log(d5.setMinutes(30));
+
+/* The setMonth() Method
+The setMonth() method sets the month of a date object. In this example to January:
+
+Example */
+
+const d4 = new Date();
+console.log(d4.setMonth(0));
+
+/* The setSeconds() Method
+The setSeconds() method sets the seconds of a date object. In this example to 59:
+
+Example */
+
+const d3 = new Date();
+console.log(d3.setSeconds(59));
