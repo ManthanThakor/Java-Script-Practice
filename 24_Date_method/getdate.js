f472baaf53cd871076eeaa2cb75ef1ceceea659f
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-28 09:35:43 +0530

    Date formate 4

--- 24_Date_method/getdate.js
+++ 24_Date_method/getdate.js
@@ -85,3 +85,75 @@ Examples */
 const d6 = new Date("2021-03-25");
 let xx = d6.getHours();
 console.log(xx);
+
+/* The getMinutes() Method
+The getMinutes() method returns the minutes of a date as a number (0-59):
+
+Examples */
+
+const d7 = new Date("2021-03-25");
+let yy = d7.getMinutes();
+console.log(yy);
+
+/* The getSeconds() Method
+The getSeconds() method returns the seconds of a date as a number (0-59):
+
+Examples */
+
+const d8 = new Date("2021-03-25");
+let zz = d8.getSeconds();
+console.log(zz);
+
+
+/* The getMilliseconds() Method
+The getMilliseconds() method returns the milliseconds of a date as a number (0-999):
+
+Examples */
+
+const d9 = new Date("2021-03-25");
+let mm = d9.getMilliseconds();
+console.log(mm);
+
+/* The getTime() Method
+The getTime() method returns the milliseconds since January 1, 1970:
+
+Examples */
+
+const d10 = new Date("2021-03-25");
+let tt = d10.getTime();
+console.log(tt);
+
+/* The getDate() Method
+The getDate() method returns the day of a date as a number (1-31):
+
+Examples */
+
+const d11 = new Date("2021-03-25");
+let dd = d11.getDate();
+console.log(dd);
+
+/* The getDay() Method
+The getDay() method returns the day of the week as a number (0-6):
+
+Examples */
+
+const d12 = new Date("2021-03-25");
+let dda = d12.getDay();
+console.log(dda);
+
+
+/* UTC Date Get Methods
+Method	Same As	Description
+getUTCDate()	getDate()	Returns the UTC date
+getUTCFullYear()	getFullYear()	Returns the UTC year
+getUTCMonth()	getMonth()	Returns the UTC month
+getUTCDay()	getDay()	Returns the UTC day
+getUTCHours()	getHours()	Returns the UTC hour
+getUTCMinutes()	getMinutes()	Returns the UTC minutes
+getUTCSeconds()	getSeconds()	Returns the UTC seconds
+getUTCMilliseconds()	getMilliseconds()	Returns the UTC milliseconds
+UTC methods use UTC time (Coordinated Universal Time).
+
+UTC time is the same as GMT (Greenwich Mean Time).
+
+The difference between Local time and UTC time can be up to 24 hours. */
