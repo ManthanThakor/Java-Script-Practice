270e657c605a8fe61ae3eb421c79f31815a8776b
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-28 10:07:12 +0530

    number

---
+++ 28_number/index.js
@@ -0,0 +1,4 @@
+// https://www.w3schools.com/js/js_numbers.asp
+// https://www.w3schools.com/js/js_bigint.asp
+// https://www.w3schools.com/js/js_number_methods.asp
+// https://www.w3schools.com/js/js_number_methods.asp
