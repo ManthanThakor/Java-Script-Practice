a373efa7a283ef8743621cacbc50bc7466c4f479
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-29 09:41:38 +0530

    DOM PART - 1.5

--- 30_DOM/30.1_DOM PART-1/script.js
+++ 30_DOM/30.1_DOM PART-1/script.js
@@ -43,3 +43,31 @@ The W3C DOM standard is separated into 3 different parts:
 Core DOM - standard model for all document types
 XML DOM - standard model for XML documents
 HTML DOM - standard model for HTML documents */
+
+
+
+
+
+
+
+
+
+
+
+// =============HTML DOM Methods===================
+
+/* JavaScript - HTML DOM Methods
+HTML DOM methods are actions you can perform (on HTML Elements).
+
+HTML DOM properties are values (of HTML Elements) that you can set or change.
+
+The DOM Programming Interface
+The HTML DOM can be accessed with JavaScript (and with other programming languages).
+
+In the DOM, all HTML elements are defined as objects.
+
+The programming interface is the properties and methods of each object.
+
+A property is a value that you can get or set (like changing the content of an HTML element).
+
+A method is an action you can do (like add or deleting an HTML element). */
