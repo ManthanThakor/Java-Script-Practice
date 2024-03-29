e5fc96cd2bd930c68db7a96fa210207cdd0a4bae
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-29 10:10:32 +0530

    DOM PART - 1.8

--- 30_DOM/30.1_DOM PART-1/script.js
+++ 30_DOM/30.1_DOM PART-1/script.js
@@ -68,6 +68,40 @@ const elementBySelector = document.querySelector('.myClass');
 
 
 /* 2. Changing HTML Elements
+Property	|    Description
 
+element.innerHTML =  new html content | Change the inner HTML of an element
+
+element.attribute = new value | Change the attribute value of an HTML element
+
+element.style.property = new style | Change the style property of an HTML element
+
+
+Method    |   Description
+
+element.setAttribute(attribute, value)	| Change the attribute value of an HTML element */
+
+
+/* 3. Adding and Deleting Elements
+
+Method	    |   Description
+
+document.createElement(element)	   |    Create an HTML element
+
+document.removeChild(element)   |	Remove an HTML element
+
+document.appendChild(element)   |	Add an HTML element
+
+document.replaceChild(new, old) |	Replace an HTML element
+
+document.write(text)    |	Write into the HTML output stream
+
+ */
+
+/* Adding Events Handlers
+
+Method  |	Description
+
+document.getElementById(id).onclick = function(){code}	    |   Adding event handler code to an onclick event
 
 */
